be4c6948534796f3fa46afdc2b052ed57a4aa10d
jondeal <87876334+jondeal@users.noreply.github.com>
2023-02-17 11:36:10 -0800

    Frame changed to more subtle radial gradient. Knobs re-added and sized dynamically.  Prevented overflow on mouseover of sketch area.

--- eas.js
+++ eas.js
@@ -5,10 +5,19 @@ let sideSquares = 16;
 const squareSize = (viewHeight - frameThickness) / sideSquares;
 const container = document.getElementById("container");
 const frame = document.getElementById("frame");
+const knobs = document.getElementById("knobs");
+const leftKnob = document.getElementById("left-knob");
+const rightKnob = document.getElementById("right-knob");
 
-frame.style.height = viewHeight + "px";
-frame.style.width = frame.style.height;
+frame.style.maxHeight = viewHeight + "px";
+frame.style.width = frame.style.maxHeight;
 container.style.width = sideSquares * squareSize + "px";
+container.style.marginTop = frameSize + "px";
+knobs.style.width = frame.style.width;
+leftKnob.style.height = frameSize + "px";
+leftKnob.style.width = frameSize + "px";
+rightKnob.style.height = frameSize + "px";
+rightKnob.style.width = frameSize + "px";
 
 function changeColor() {
     this.style.backgroundColor = "black";
@@ -27,38 +36,36 @@ while (columns < sideSquares) {
     columns++;
 }
 
-// leftKnob = document.getElementById("left-knob");
-// rightKnob = document.getElementById("right-knob");
 
-// let leftKnobRotation = 0;
-// let rightKnobRotation = 0;
 
-// let oldX = 0;
-// let oldY = 0;
+let leftKnobRotation = 0;
+let rightKnobRotation = 0;
 
-// function rotateKnobs(event) {
-//     if (event.clientX > oldX) {
-//         leftKnobRotation += 5;
-//     } else if (event.clientX < oldX) {
-//         leftKnobRotation -= 5;
-//     } else {
-//         leftKnobRotation += 0;
-//     }
+let oldX = 0;
+let oldY = 0;
 
-//     if (event.clientY < oldY) {
-//         rightKnobRotation += 5;
-//     } else if (event.clientY > oldY) {
-//         rightKnobRotation -= 5;
-//     } else {
-//         rightKnobRotation += 0;
-//     }
+function rotateKnobs(event) {
+    if (event.clientX > oldX) {
+        leftKnobRotation += 5;
+    } else if (event.clientX < oldX) {
+        leftKnobRotation -= 5;
+    } else {
+        leftKnobRotation += 0;
+    }
 
-//     leftKnob.style.transform = "rotate(" + leftKnobRotation + "deg)";
-//     rightKnob.style.transform = "rotate(" + rightKnobRotation + "deg)";
+    if (event.clientY < oldY) {
+        rightKnobRotation += 5;
+    } else if (event.clientY > oldY) {
+        rightKnobRotation -= 5;
+    } else {
+        rightKnobRotation += 0;
+    }
 
-//     oldX = event.clientX;
-//     oldY = event.clientY;
-// }
+    leftKnob.style.transform = "rotate(" + leftKnobRotation + "deg)";
+    rightKnob.style.transform = "rotate(" + rightKnobRotation + "deg)";
 
-// container.style.margin = (frameSize) + "px";
-// container.addEventListener("mousemove", rotateKnobs);
+    oldX = event.clientX;
+    oldY = event.clientY;
+}
+
+container.addEventListener("mousemove", rotateKnobs);
